4e5faadfe579e2db7bb0c63b898d67a8e2786dc4
Anton Chmyrov <achmyrov@gmail.com>
2020-05-28 14:48:21 +0200

    changed: app icon, app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useContext } from 'react';
 import TopBar from './components/TopBar';
+import { Helmet } from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
 import { Light, Dark, DarkModeContext } from './styled/Themes';
 import { StyledBody } from './styled/StyledComponents';
@@ -10,21 +11,28 @@ import { SetDarkModeConst } from './components/tetris/utils/constants';
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <DarkModeContext.Provider
-      value={{
-        DarkMode: darkMode,
-        SetDarkMode: (darkMode: boolean) => {
-          setDarkMode(darkMode);
-          SetDarkModeConst(darkMode);
-        },
-      }}
-    >
-      <ThemeProvider theme={darkMode ? Dark : Light}>
-        <GlobalTableStyles />
-        <StyledBody>
-          <TopBar />
-        </StyledBody>
-      </ThemeProvider>
-    </DarkModeContext.Provider>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Desktop APP</title>
+        <link rel="canonical" href="http://achmyrov.com" />
+      </Helmet>
+      <DarkModeContext.Provider
+        value={{
+          DarkMode: darkMode,
+          SetDarkMode: (darkMode: boolean) => {
+            setDarkMode(darkMode);
+            SetDarkModeConst(darkMode);
+          },
+        }}
+      >
+        <ThemeProvider theme={darkMode ? Dark : Light}>
+          <GlobalTableStyles />
+          <StyledBody>
+            <TopBar />
+          </StyledBody>
+        </ThemeProvider>
+      </DarkModeContext.Provider>
+    </div>
   );
 }
